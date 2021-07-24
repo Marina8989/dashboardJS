471a1fffafcab827aa3b6efb4cd60bebf1acfffe
Marina8989 <marina.mill209@gmail.com>
2021-07-23 18:59:03 -0700

    charts js

--- main.js
+++ main.js
@@ -22,9 +22,9 @@ let myChart = document.getElementById('myChart').getContext('2d');
                 datasets: [{
                     label: 'TRAFFIC',
                     data: [2800, 2825, 2850, 2875, 2900, 2925, 2950, 2975, 3000],
-                    borderColor: ['rgb(75, 192, 192)', 'rgb(55, 134, 164)', 'rgb(125, 92, 92)', 'rgb(84, 25, 183)', 'rgb(75, 36, 192)', 'rgb(92, 92, 125)', 'rgb(50, 112, 142)'],
+                    borderColor: 'rgb(75, 192, 192)',
                     tension: 0.1
                 }],
-   
+                
             }
         })
